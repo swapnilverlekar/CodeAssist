06c506836631a388d0a40735dfd11e913fd0b1cb
swapv24 <swapnilverlekar24@gmail.com>
2022-12-27 16:59:44 -0500

    Updated the server URL

--- client/Script.js
+++ client/Script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch message from the server
-  const response = await  fetch('http://localhost:5001',{
+  const response = await  fetch('https://codeassist-alia.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type': 'application/json'
